b4753c300be64d5d93f495f753cfa3c42f92bb3a
HinnyTsang <mhtsangab@connect.ust.hk>
2022-03-05 19:10:48 +0800

    update client welcome message and mcnugget

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,11 +6,12 @@ $(function() {
     $("#footer-placeholder").load("../includes/footer.html");
 });
 
-function reveal() {
 
+
+// To check the scroll position on page load
+function reveal() {
     // reveals = all elements with class = "reveal".
     var reveals = document.querySelectorAll(".reveal");
-    // console.log("reveal");
     // update for all elements.
     for (var i = 0; i < reveals.length; i++) {
         var windowHeight = window.innerHeight;
@@ -29,7 +30,23 @@ window.addEventListener("scroll", reveal);
 reveal();
 
 
+// fade in when the page load.
 jQuery('body').css('display', 'none');
 jQuery(document).ready(function() {
     jQuery('body').fadeIn(700);
+});
+
+
+
+// get ip of the client's 
+// see https://www.abstractapi.com/
+let apiKey = '5f3960b5491a49b7b612a8295fd6f988';
+let abstract_api = 'https://ipgeolocation.abstractapi.com/v1/?api_key=';
+$.getJSON(abstract_api + apiKey, function(data) {
+    // console.log(JSON.stringify(data, null, 2));
+    // console.log(data.country);
+    // print out the ip adress of the client.
+    let target = document.getElementById("hello");
+    target.innerText = "Welcome my " + data.country + " friends";
+
 });
