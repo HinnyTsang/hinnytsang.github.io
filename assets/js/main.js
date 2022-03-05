c1feb1fa940757d061419790ba1da5f32e60da6d
HinnyTsang <mhtsangab@connect.ust.hk>
2022-03-05 20:12:14 +0800

    nav bar fix size

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,7 @@ function reveal() {
     for (var i = 0; i < reveals.length; i++) {
         var windowHeight = window.innerHeight;
         var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = windowHeight / 4;
+        var elementVisible = windowHeight / 10;
         if (elementTop < windowHeight - elementVisible) {
             reveals[i].classList.add("active");
         } else {
