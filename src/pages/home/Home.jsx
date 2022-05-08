3bdb5f1b5735d8ad77d66cc2133c14adc1e2a24b
HinnyTsang <mhtsangab@connect.ust.hk>
2022-05-09 05:08:51 +0800

    update card for slime simulation

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Row, Col, Card, CardGroup } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { Page , SubTitle } from '../../components';
+import { Page, SubTitle } from '../../components';
 import './Home.css'
 
 const Home = () => {
@@ -11,14 +11,14 @@ const Home = () => {
 
   return (
     <Page title={<p>MYstical - Universe</p>}>
-      
+
       <Row className='description'>
         <Col>Hi, welcome to my universe.
           This wepage is my personal webpage created with React.js as a learning project.
           I would also upload some interesting things here. Enjoy!
         </Col>
       </Row>
-      
+
       <SubTitle>Whats New?</SubTitle>
       <CardGroup className='card-group mt-5'>
         <Card
@@ -30,6 +30,20 @@ const Home = () => {
           <Card.Body>
             <Card.Title>
               Real time star simulation
+            </Card.Title>
+            <Card.Text>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card
+          as={Link} to='../simulation/'
+          className="card"
+        >
+          <Card.Img variant="top"
+            src="/images/slime-icon.png" alt="sph" className='card--image' />
+          <Card.Body>
+            <Card.Title>
+              Real time slime mold simulation
             </Card.Title>
             <Card.Text>
             </Card.Text>
