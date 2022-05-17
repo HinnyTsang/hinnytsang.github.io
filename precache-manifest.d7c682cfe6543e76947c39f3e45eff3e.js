1edc3c1e83e04a536e66e50fb60002948c6861b6
hinny <hinny@hinnytsang.com>
2022-05-18 01:08:23 +0800

    Updates

---
+++ precache-manifest.d7c682cfe6543e76947c39f3e45eff3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "40acc4f2450e95389855",
+    "url": "/static/css/main.4ef1d958.chunk.css"
+  },
+  {
+    "revision": "40acc4f2450e95389855",
+    "url": "/static/js/main.40acc4f2.chunk.js"
+  },
+  {
+    "revision": "5b23065dc5398409f3c6",
+    "url": "/static/js/1.5b23065d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8968bc5b9ead9e1f7022336e59c86f5e",
+    "url": "/index.html"
+  }
+];
