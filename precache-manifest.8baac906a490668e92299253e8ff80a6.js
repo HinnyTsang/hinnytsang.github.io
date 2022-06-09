59e00f53891d9e72cb3c73635d75175ff8d74af5
hinny <hinny@hinnytsang.com>
2022-06-09 18:40:09 +0800

    Updates

---
+++ precache-manifest.8baac906a490668e92299253e8ff80a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c11e3058677c32c842505b16760cd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a47291cdd9ea2ca61f8b",
+    "url": "/static/css/main.abb3067d.chunk.css"
+  },
+  {
+    "revision": "63488fd8f0331e6be273",
+    "url": "/static/js/2.99532915.chunk.js"
+  },
+  {
+    "revision": "ce35da599936da27e4c12b090a4eb5e2",
+    "url": "/static/js/2.99532915.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a47291cdd9ea2ca61f8b",
+    "url": "/static/js/main.194229dc.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.194229dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ace7223dff5d331bdfa8",
+    "url": "/static/js/runtime-main.1d52cc1d.js"
+  }
+]);
