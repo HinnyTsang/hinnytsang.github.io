c3cafa72b5b1e09b4612298a5739c31d6aee3960
Hinny Tsang <mhtsangab@connect.ust.hk>
2026-02-08 19:09:24 +0000

    Config next to generate statis folder

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
+	output: "export",
 	reactCompiler: true,
+	images: {
+		unoptimized: true,
+	},
 };
 
 export default nextConfig;
