ec0232b2ed22b7063433a4dfb2db852cc918e403
Hinny Tsang <mhtsangab@connect.ust.hk>
2026-02-09 22:57:48 +0000

    Add meta data session

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,10 @@
+import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "My Universe",
+};
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
